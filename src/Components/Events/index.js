288b7dfc6eb2987d0954a77478d4ebfd753e3f1e
Chovatiya Viral <vchovatiya992@gmail.com>
2022-03-13 14:53:52 +0530

    create register party event form

--- src/Components/Events/index.js
+++ src/Components/Events/index.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../Layout'
+import axios from 'axios';
+import { baseURL } from '../../helper';
 
 export default function Events() {
+
+    const [eventData, setEventData] = useState([]);
+    const auth = localStorage.getItem('token')
+
+    useEffect(() => {
+        fetchEventData()
+    }, []);
+
+    const fetchEventData = async () => {
+        if (auth) {
+            try {
+                await axios.get(baseURL + '/partyEvents', {
+                    headers: {
+                        'x-access-token': `${auth}`,
+                    }
+                })
+                    .then((res) => {
+                        if (res.status === 200) {
+                            setEventData(res.data)
+                        }
+                    })
+
+            } catch (error) {
+                console.log("err", error);
+            }
+        }
+    }
+
     return (
         <Layout>
-            
+            {
+                eventData && eventData.length ?
+                    <>
+                        {
+                            eventData.map((item, index) => {
+                                return (
+                                    <div key={index}>
+                                        <img src={item.logo} alt="logo image" />
+                                    </div>
+                                )
+                            })
+                        }
+                    </>
+                    : null
+            }
         </Layout>
     )
 }
--- src/Components/PartyEventRegister/index.js
+++ src/Components/PartyEventRegister/index.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles, TextField, Button } from '@material-ui/core'
 import Layout from '../../Layout'
+import axios from 'axios';
+import { baseURL } from '../../helper';
+import { useNavigate } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
     eventForm: {
@@ -11,19 +14,92 @@ const useStyles = makeStyles(theme => ({
             marginBottom: '10px'
         }
     },
-    buttons:{
-        display:'flex',
-        margin:'10px',
-        justifyContent:'center',
+    buttons: {
+        display: 'flex',
+        margin: '10px',
+        justifyContent: 'center',
 
-        '& .MuiButtonBase-root':{
-            margin:'0px 10px'
+        '& .MuiButtonBase-root': {
+            margin: '0px 10px'
         }
     }
 }));
 
 export default function PartyEventRegister() {
+    const navigate = useNavigate();
+
+    const [partyEventData, setPartyEventData] = useState({
+        // poster_img: "",
+        event_name: "",
+        state: "",
+        date: "",
+        logo: "",
+        // company_logo: "",
+        company_name: "",
+        sponsor: ""
+    })
+
     const classes = useStyles();
+    const auth = localStorage.getItem('token')
+
+    const handleChange = (e) => {
+        setPartyEventData({
+            ...partyEventData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleImageChange = (e) => {
+        setPartyEventData({
+            ...partyEventData,
+            [e.target.name]: e.target.files[0]
+        })
+    }
+
+    const handleResetData = () => {
+        setPartyEventData({
+            // poster_img: "",
+            event_name: "",
+            state: "",
+            date: "",
+            logo: "",
+            // company_logo: "",
+            company_name: "",
+            sponsor: ""
+        })
+    }
+    console.log("partyEventData", partyEventData);
+    const handleSubmit = async () => {
+        var formData = new FormData();
+        if (partyEventData.event_name && partyEventData.state && partyEventData.date && partyEventData.company_name && partyEventData.sponsor) {
+            formData.append("logo", partyEventData.logo);
+            formData.append("event_name", partyEventData.event_name);
+            formData.append("state", partyEventData.state);
+            formData.append("date", partyEventData.date);
+            formData.append("company_name", partyEventData.company_name);
+            formData.append("sponsor", partyEventData.sponsor);
+            try {
+                await axios.post(baseURL + '/partyEvents', formData, {
+                    headers: {
+                        'x-access-token': `${auth}`,
+                        "content_type": "application/json"
+                    }
+                })
+                    .then((res) => {
+                        if (res.status === 200) {
+                            navigate('/party-event');
+                            handleResetData();
+                        }
+                    })
+
+            } catch (error) {
+                console.log("err", error);
+                handleResetData();
+            }
+        }
+
+    }
+
     return (
         <Layout>
             <form className={classes.eventForm}>
@@ -35,6 +111,7 @@ export default function PartyEventRegister() {
                     name="event_name"
                     type="text"
                     variant="standard"
+                    onChange={handleChange}
                 />
                 <div style={{ display: 'inline-grid' }}>
                     <label>Logo</label>
@@ -45,6 +122,7 @@ export default function PartyEventRegister() {
                         name="logo"
                         type="file"
                         variant="standard"
+                        onChange={handleImageChange}
                     />
                 </div>
                 <TextField
@@ -55,18 +133,20 @@ export default function PartyEventRegister() {
                     name="company_name"
                     type="text"
                     variant="standard"
+                    onChange={handleChange}
                 />
-                <div style={{ display: 'inline-grid' }}>
-                    <label>Logo</label>
+                {/* <div style={{ display: 'inline-grid' }}>
+                    <label>Company Logo</label>
                     <TextField
                         required
                         id="standard-required"
                         // label="Company Logo required"
+                        onChange={handleImageChange}
                         name="company_logo"
                         type="file"
                         variant="standard"
                     />
-                </div>
+                </div> */}
                 <TextField
                     required
                     id="standard-required"
@@ -75,15 +155,17 @@ export default function PartyEventRegister() {
                     name="state"
                     type="text"
                     variant="standard"
+                    onChange={handleChange}
                 />
                 <TextField
                     required
                     id="standard-required"
-                    label="state"
+                    label="sponsor"
                     // defaultValue="Enter state"
-                    name="state"
+                    name="sponsor"
                     type="text"
                     variant="standard"
+                    onChange={handleChange}
                 />
                 <TextField
                     required
@@ -93,25 +175,27 @@ export default function PartyEventRegister() {
                     defaultValue=""
                     type="date"
                     variant="standard"
+                    onChange={handleChange}
                 />
 
 
-                <div style={{ display: 'inline-grid' }}>
-                    <label>Logo</label>
+                {/* <div style={{ display: 'inline-grid' }}>
+                    <label>Poster Image</label>
                     <TextField
                         required
                         id="standard-required"
                         // label="postor image is required"
                         name="poster_img"
                         type="file"
+                        onChange={handleImageChange}
                         variant="standard"
                     />
-                </div>
+                </div> */}
                 <div className={classes.buttons}>
                     <Button type="button" variant="contained">
                         Cancel
                     </Button>
-                    <Button type="button" variant="contained" color="primary" >
+                    <Button type="button" variant="contained" color="primary" onClick={handleSubmit}>
                         Submit
                     </Button>
                 </div>
